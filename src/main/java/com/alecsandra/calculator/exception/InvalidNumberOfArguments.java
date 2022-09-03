0f798398515a435c44168849b201344117c7ea57
alecsandrapetruescu <alecsandra.petruescu@gmail.com>
2022-09-03 16:00:24 +0300

    fix[java:S1165] - Made fields final

--- src/main/java/com/alecsandra/calculator/exception/InvalidNumberOfArguments.java
+++ src/main/java/com/alecsandra/calculator/exception/InvalidNumberOfArguments.java
@@ -1,7 +1,7 @@
 package com.alecsandra.calculator.exception;
 
 public class InvalidNumberOfArguments extends Exception {
-    private static String NR_OF_ARGUMENTS = "#numberOfArguments#";
+    private static final String NR_OF_ARGUMENTS = "#numberOfArguments#";
     private static String message = "Invalid number of arguments : " + NR_OF_ARGUMENTS + ", minimum 3 arguments";
     public InvalidNumberOfArguments(int numberOfArguments) {
         super(message.replaceAll(NR_OF_ARGUMENTS, String.valueOf(numberOfArguments)));
--- src/main/java/com/alecsandra/calculator/exception/InvalidOperation.java
+++ src/main/java/com/alecsandra/calculator/exception/InvalidOperation.java
@@ -1,7 +1,7 @@
 package com.alecsandra.calculator.exception;
 
 public class InvalidOperation extends Exception {
-    private static String OPERATION = "#operation#";
+    private static final String OPERATION = "#operation#";
     private static String MESSAGE = "Invalid operation : " + OPERATION ;
 
     public InvalidOperation(String operation) {
